fa808c1a16e467e854c97aba8955f01d4ea5b2c4
califken <califazken@gmail.com>
2018-04-10 05:31:36 -0700

    Update projectapp.conventions.js

--- projectapp.conventions.js
+++ projectapp.conventions.js
@@ -1,28 +1,36 @@
-var googleAPIkey = "AIzaSyBQUeDjbwpS8YCApba7OFuvqwdKRFCsvmo";
-var mapaddress = "Phoenix, AZ";
-var endpoint =
-  "https://maps.googleapis.com/maps/api/geocode/json?address=" + mapaddress + "&key=" + googleAPIkey;
+// Source: github.com/kcaple/project-app-conventions
+// Author: Kenneth Caple
+// Update: 1.0
+(function($) {
 
-function queryMapsAPI() {
-  this.endpoint = endpoint;
-  $.ajax({
-    url: this.endpoint,
-    type: "GET", // The HTTP Method
-    data: {}, // Additional parameters here
-    dataType: "json",
-    error: function(err) {
-      console.log(err);
-    }
-    /**,
-    beforeSend: function(xhr) {
-      xhr.setRequestHeader("X-Mashape-Authorization", mashapeauthkey);
-    }**/
-  }).done(function(data) {
-   console.log(data);
-    var latitude = data.results[0].geometry.location.lat;
-    var longitude = data.results[0].geometry.location.lng;
-    $("#result").html("Search for: " + mapaddress + "<hr>" + "Lat: " + latitude + ", Lng: " + longitude);
-  });
-}
+        var googleAPIkey = "AIzaSyBQUeDjbwpS8YCApba7OFuvqwdKRFCsvmo";
+        var mapaddress = "Phoenix, AZ";
+        var endpoint =
+            "https://maps.googleapis.com/maps/api/geocode/json?address=" + mapaddress + "&key=" + googleAPIkey;
 
-queryMapsAPI();
+        $.fn.queryMapsAPI = function(content) {
+
+            this.endpoint = endpoint;
+            $.ajax({
+                url: this.endpoint,
+                type: "GET", // The HTTP Method
+                data: {}, // Additional parameters here
+                dataType: "json",
+                error: function(err) {
+                    console.log(err);
+                }
+                /**,
+                beforeSend: function(xhr) {
+                  xhr.setRequestHeader("X-Mashape-Authorization", mashapeauthkey);
+                }**/
+            }).done(function(data) {
+                console.log(data);
+                var latitude = data.results[0].geometry.location.lat;
+                var longitude = data.results[0].geometry.location.lng;
+                $("#result").html("Search for: " + mapaddress + "<hr>" + "Lat: " + latitude + ", Lng: " + longitude);
+            });
+        }
+
+    };
+
+})(jQuery);
